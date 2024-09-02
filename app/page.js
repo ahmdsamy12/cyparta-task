215e95d563f1de6c72e067147427a217f1c81f3f
ahmdsamy12 <ahmdsamy12@gmail.com>
2024-09-02 17:11:52 +0300

    update3

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
 const Home = () => {
   return (
     <>
-      <div className="text-2xl font-bold">Welcome to the Dashboard</div>
-      <p className="mt-4">This is the main content area.</p>
+      <DashboardLayout>
+        <div className="text-2xl font-bold">Welcome to the Dashboard</div>
+        <p className="mt-4">This is the main content area.</p>
+      </DashboardLayout>
     </>
   );
 };
